0f8c55080a9f60829bf6e3051559fa15d10a1413
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-09-12 17:16:50 -0500

    socket connection automation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// Initialize Socket Connection
+const docHost = document.location.host;
+let socket;
+if (docHost == "") {
+    socket = io('http://localhost:8000');
+} else {
+    socket = io("buzz-contest-server.herokuapp.com");
+}
+
+// 
+
 socket.on("user-join", msg => {
     const msgBox = document.createElement("div");
     msgBox.innerHTML=msg;
